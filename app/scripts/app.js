9279fc01137612eabc06d203e5753554eca4245f
Lee Penkman <leepenkman@gmail.com>
2015-04-14 08:51:10 +1200

    Add ring icons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,7 @@
 (function (document) {
   'use strict';
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
-  app.appName = 'Yo, Polymer App!';
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
