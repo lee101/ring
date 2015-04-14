090e10b3d4aedd333282b0bb1e5eb647a21fb364
Lee Penkman <leepenkman@gmail.com>
2015-04-14 18:16:05 +1200

    format

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,16 @@
+
 (function (document) {
-  'use strict';
+    'use strict';
 
-  var app = document.querySelector('#app');
+    var app = document.querySelector('#app');
 
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
-  app.addEventListener('template-bound', function() {
-    //console.log('Our app is ready to rock!');
-  });
-    $('#gallery-tiles').justifiedGallery()
+    // Listen for template bound event to know when bindings
+    // have resolved and content has been stamped to the page
+    app.addEventListener('template-bound', function () {
+        //console.log('Our app is ready to rock!');
+    });
+
+    $('#gallery-tiles').justifiedGallery();
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
