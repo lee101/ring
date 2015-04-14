86f32b2bafc8e36ef92b1cda8d6ff0837401be6d
Lee Penkman <leepenkman@gmail.com>
2015-04-14 18:03:02 +1200

    Add logo + failed attempt at adding a image gallery component because of shadowdom

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,7 @@
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('template-bound', function() {
-    console.log('Our app is ready to rock!');
+    //console.log('Our app is ready to rock!');
   });
 
 // wrap document so it plays nice with other libraries
