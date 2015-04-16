f992497430ff660a476944892b9e47626d2d2243
Lee Penkman <leepenkman@gmail.com>
2015-04-16 18:01:15 +1200

    nstSlider + polymer tooltips

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,8 +17,8 @@
             "right_grip_selector": ".rightGrip",
             "value_bar_selector": ".bar",
             "value_changed_callback": function (cause, leftValue, rightValue) {
-                $(this).parent().find('.leftLabel').text( '$ ' + leftValue);
-                $(this).parent().find('.rightLabel').text( '$ ' + rightValue);
+                $(this).parent().find('.leftLabel').attr('label', '$ ' + leftValue);
+                $(this).parent().find('.rightLabel').attr('label', '$ ' + rightValue);
             }
         });
     });
