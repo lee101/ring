86c5c83a1e552c2ef82e7285a3824c0dfa0fb908
Lee Penkman <leepenkman@gmail.com>
2015-04-20 10:12:25 +1200

    Use a histogram of prices for the range slider

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,24 +6,50 @@
     // Listen for template bound event to know when bindings
     // have resolved and content has been stamped to the page
     app.addEventListener('template-bound', function () {
-        //console.log('Our app is ready to rock!');
+
         $('#gallery-tiles').justifiedGallery({
             rowHeight: 300,
             lastRow: 'nojustify',
             margins: 2
         });
+
+        var priceHistogram = [
+            0,
+            10,
+            25,
+            50,
+            75,
+            100,
+            150,
+            200,
+            250,
+            300,
+            350,
+            400,
+            500,
+            750,
+            1000,
+            1500,
+            2000,
+            3000,
+            4000,
+            5000,
+            10000
+        ];
         $('#price-range').nstSlider({
             "left_grip_selector": ".leftGrip",
             "right_grip_selector": ".rightGrip",
             "value_bar_selector": ".bar",
             "value_changed_callback": function (cause, leftValue, rightValue) {
-                $(this).parent().find('.leftLabel').attr('label', '$ ' + leftValue);
+                $(this).parent().find('.leftLabel').attr('label', '$ ' + priceHistogram[leftValue]);
+                var rightText = priceHistogram[rightValue];
                 if (rightValue == $('#price-range').data('range_max')) {
-                    rightValue += '+'
+                    rightText += '+'
                 }
-                $(this).parent().find('.rightLabel').attr('label', '$ ' + rightValue);
+
+                $(this).parent().find('.rightLabel').attr('label', '$ ' + rightText);
             }
-        }).set_step_histogram([10,20,50,99,500,5000]);
+        });
     });
 
 
