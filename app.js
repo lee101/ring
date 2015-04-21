6fc7bc4e3d488f744956e9542949f113ef91f70d
Lee Penkman <leepenkman@gmail.com>
2015-04-21 18:00:40 +1200

    WIP add express

--- app.js
+++ app.js
@@ -15,18 +15,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/app/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-
-proxySubdomain = require('./proxy');
-
-app.use(proxySubdomain);
-
 
 app.use('/', routes);
 app.use('/users', users);
