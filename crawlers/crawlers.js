ef52b40ba0a34fe5978bdf41ff6ce96e259004bb
Lee Penkman <leepenkman@gmail.com>
2015-05-05 09:17:06 +1200

    WIP Micheal hill crawling

--- crawlers/crawlers.js
+++ crawlers/crawlers.js
@@ -1,4 +1,5 @@
 var cheerio = require('cheerio');
+var request = require('request');
 
 
 var crawlers = (function () {
@@ -11,12 +12,51 @@ var crawlers = (function () {
      bump start by sz (size) to paginate
       */
 
-    self.michealHill = function () {
-        ;
-    };
+    self.michealHill = (function () {
+        var mHillSelf = {};
+        var pagesProductsCount = 0;
+        var start = 1;
+        var step = 40;
+
+        mHillSelf.getPages = function (){
+            request('http://www.michaelhill.co.nz/jewellery/rings?start='+start+'&format=page-element&sz=40&', function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var $content = cheerio.load(body);
+                    var pageUrls = mHillSelf.getPageUrls(body);
+                    mHillSelf.parsePageUrls(body);
+                    if (pageUrls.length == step) {
+                        start += step;
+                        getPages();
+                    }
+                } else {
+                    return console.log(error);
+
+                }
+            })
+        };
+
+        mHillSelf.parsePageUrls = function ($page) {}
+        mHillSelf.getPageUrls = function ($page) {
+            var pageUrls = [];
+            $page('li').each(function (i, obj) {
+                pageUrls.push($page(obj).find('.thumb-link').attr('href'));
+            });
+            return pageUrls;
+        };
+        return mHillSelf;
+    })();
+
     self.getTitle = function (dom) {
         var title = dom('[property="og:title"]').attr('content');
         return title;
+    };
+    self.getDescription = function (dom) {
+        var ogDescription = dom('[property="og:description"]').attr('content');
+        return ogDescription;
+    };
+    self.getImage = function (dom) {
+        var ogImage = dom('[property="og:image"]').attr('content');
+        return ogImage;
     };
 
 
--- test/crawlers/test_crawlers.js
+++ test/crawlers/test_crawlers.js
@@ -6,26 +6,47 @@ var cheerio = require('cheerio');
 var crawlers = require('../../crawlers/crawlers');
 
 var mHillDetailRingSilver;
+var mHillRingGalleryPage;
 describe('setup', function () {
 
-    it('sets up', function (done) {
-        fs.readFile('./test/crawlers/micheall-hill-detail-ring-silver.html', 'utf8', function (err, data) {
+    it('sets up', function () {
+        fs.readFile('./test/crawlers/micheal-hill-detail-ring-silver.html', 'utf8', function (err, data) {
             if (err) {
                 return console.log(err);
             }
             mHillDetailRingSilver = cheerio.load(data);
-            done();
+        });
+        fs.readFile('./test/crawlers/micheal-hill-ring-gallery-page.html', 'utf8', function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            mHillRingGalleryPage = cheerio.load(data);
         });
     })
 });
 
 describe('crawlers', function () {
 
-    describe('#getTitle(dom)', function () {
+    it('#getTitle(dom) should use og:title', function () {
+        var title = crawlers.getTitle(mHillDetailRingSilver);
+        expect(title).to.equal('Ring with Enhanced Black Diamonds in Sterling Silver')
+    });
 
-        it('should use og:title', function () {
-            var title = crawlers.getTitle(mHillDetailRingSilver);
-            expect(title).to.equal('Ring with Enhanced Black Diamonds in Sterling Silver')
-        })
+    it('#getDescription(dom) should use og:description', function () {
+        var title = crawlers.getDescription(mHillDetailRingSilver);
+        expect(title).to.equal('Showcase your flair for fashion with this sterling silver snake ring. Created to look like a snake coiled around your finger, the scale patterned body and  enhanced black diamond details bring this piece to life.')
+    });
+    it('#getImage(dom) should use og:image', function () {
+        var title = crawlers.getImage(mHillDetailRingSilver);
+        expect(title).to.equal('http://demandware.edgesuite.net/aanc_prd/on/demandware.static/Sites-MichaelHillNZ-Site/Sites-MHJ_Master/en_NZ/v1430441345851/hi-res/12322275_1.jpg')
     })
+    describe('micheal hill', function () {
+
+        it('#getPageUrls(dom) should use og:image', function () {
+            var pageUrls = crawlers.michealHill.getPageUrls(mHillRingGalleryPage)
+            expect(pageUrls.length).to.equal(12)
+        })
+    });
 });
+
+
