433001da783f92e406f33404f73bd68b227d70c3
Lee Penkman <leepenkman@gmail.com>
2015-05-17 11:38:16 +1200

    testing lwip

--- test/images/lwiptest.js
+++ test/images/lwiptest.js
@@ -1,12 +1,20 @@
 var lwip = require('lwip');
+var request = require('request');
 
-var tiffanyRingLarge;
 
-lwip.open('./test/images/tiffany-needs-cropping.jpeg', function (err, image) {
-    image.crop(100, 100, 200, 200, function (err, image) {
-        image.writeFile('test/images/test.jpeg', function (err) {
-            ;
+//lwip.open('./test/images/tiffany-needs-cropping.jpeg', function (err, image) {
+//    image.crop(100, 100, 200, 200, function (err, image) {
+//        image.writeFile('test/images/test.jpeg', function (err) {
+//            ;
+//        })
+//    })
+//});
+request('http://www.swarovski.com/is-bin/intershop.static/WFS/SCO-Media-Site/-/-/publicimages/CG/B2C/PROD/BestView/5033029/TileGroup0/0-0-0.jpg', function (error, response, body) {
+    lwip.open(new Buffer(body), 'jpg', function (err, image) {
+        image.crop(100, 100, 200, 200, function (err, image) {
+            image.writeFile('test/images/swarskovtest.jpeg', function (err) {
+                ;
+            })
         })
-    })
-
-});
+    });
+})
